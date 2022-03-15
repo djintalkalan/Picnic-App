8c64cc8d433d6319b87b94ee0c9f5e3ddf215dbe
Deepak Jaglan <deepak@shinewebservices.com>
2022-03-15 14:14:08 +0530

    fix

--- src/screens/GooglePlacesTextInput.tsx
+++ src/screens/GooglePlacesTextInput.tsx
@@ -1,3 +1,4 @@
+import { useFocusEffect } from '@react-navigation/native'
 import { config } from 'api'
 import { colors, Images } from 'assets'
 import { Text } from 'custom-components'
@@ -23,10 +24,16 @@ const GooglePlacesTextInput: FC<any> = (props) => {
     //     // hi: "dj"
     // })
     // console.log("navigation", props.navigation.getState(), props)
+    useFocusEffect(useCallback(() => {
+        setTimeout(() => {
+            setFocused(true)
+        }, 100);
+    }, []))
     const placeInputRef = useRef<GooglePlacesAutocompleteRef>(null);
     const dispatch = useDispatch()
 
     const [searchText, setSearchText] = useState("");
+    const [isFocused, setFocused] = useState(false);
 
     const [recentSearches] = useDatabase<Array<IRecentSearches>>("recentSearches", [])
 
@@ -70,115 +77,119 @@ const GooglePlacesTextInput: FC<any> = (props) => {
             }
         }
     }, [])
+
     return (
-        <SafeAreaViewWithStatusBar style={{ flex: 1, backgroundColor: colors.colorWhite }} >
-            <TouchableOpacity style={{ flex: 1, paddingTop: scaler(10) }} disabled >
-
-                <GooglePlacesAutocomplete
-                    ref={placeInputRef}
-                    keyboardShouldPersistTaps={'always'}
-                    placeholder={Language.search_location}
-                    minLength={3}
-                    fetchDetails={true}
-                    keepResultsAfterBlur
-                    textInputProps={{
-                        autoFocus: true,
-                        returnKeyType: 'done',
-                        placeholderTextColor: colors.colorGreyInactive,
-                        onChangeText: (text) => { setSearchText(text) },
-                        rightIcon: { type: 'font-awesome', name: 'chevron-left', color: 'black' },
-                    }}
-                    enablePoweredByContainer={false}
-                    enableHighAccuracyLocation={true}
-                    nearbyPlacesAPI={'GoogleReverseGeocoding'}
-                    GooglePlacesDetailsQuery={{
-                        types: '(cities)',
-                    }}
-                    filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']}
-                    query={{
-                        key: config.GOOGLE_MAP_API_KEY,
-                        language: 'en',
-                        types: '(cities)',
-                    }}
-                    onPress={_onPress}
-                    renderRow={_renderRow}
-                    isRowScrollable={false}
-                    debounce={500}
-                    styles={{
-                        listView: {
-                            marginTop: scaler(20),
-                            borderTopColor: "rgba(0, 0, 0, 0.06)",
-                            borderTopWidth: 1,
-                            flex: 1,
-                            // backgroundColor:'red'
-                        },
-                        row: {
-                            padding: scaler(15),
-                        },
-                        separator: {
-                            width: Dimensions.get('screen').width - scaler(40),
-                            alignSelf: 'center',
-                        },
-                        container: {
-                        },
-                        textInputContainer: {
-                            borderTopWidth: 0,
-                            borderBottomWidth: 0,
-                            marginTop: scaler(10),
-
-                            // backgroundColor:colors.colorBlack
-                        },
-                        textInput: styles.searchInput,
-                        predefinedPlacesDescription: {
-                            color: 'white',
-                        },
-                    }}
-                    currentLocation={false}
-                />
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => { NavigationService.goBack() }} style={styles.closeButton} >
-                <Image style={styles.imagePlaceholder} source={Images.ic_left} />
-            </TouchableOpacity>
-
-            {(!searchText || searchText.length == 0) &&
-                <View style={{
-                    top: scaler(60), left: 0, right: 0, position: 'absolute', flex: 1, width: '100%',
-                    marginTop: scaler(20),
-                    // borderTopColor: "rgba(0, 0, 0, 0.06)",
-                    // borderTopWidth: 2,
-                }} >
-                    <Text style={{
-                        fontWeight: '500', flex: 1, fontSize: scaler(12), color: '#9A9A9A',
-                        marginHorizontal: scaler(20),
-                        marginTop: scaler(20),
-                        marginBottom: scaler(10),
-                    }}>{Language.recent_search}</Text>
-                    <FlatList
-                        style={{ flex: 1 }}
-                        showsVerticalScrollIndicator={false}
-                        data={recentSearches ?? []}
-                        renderItem={({ item, index }) => {
-                            return <TouchableHighlight
-                                onPress={() => _onPress(item?.data, item?.details, item?.otherData)}
-                                style={{ width: '100%', padding: scaler(15) }
-                                }
-                                //   onPress={() => _onPress(rowData)}
-                                underlayColor={'#c8c7cc'}
-                            >
-                                {_renderRow(item?.data, index, Images.ic_recent_search)}
-                            </TouchableHighlight>
+        <SafeAreaViewWithStatusBar style={{ flex: 1 }} >
+            <View style={{ flex: 1 }}>
+                <TouchableOpacity style={{ flex: 1, paddingTop: scaler(10) }} disabled >
+                    {isFocused && <GooglePlacesAutocomplete
+                        ref={placeInputRef}
+                        keyboardShouldPersistTaps={'always'}
+                        placeholder={Language.search_location}
+                        minLength={3}
+                        fetchDetails={true}
+                        keepResultsAfterBlur
+                        textInputProps={{
+                            autoFocus: true,
+                            returnKeyType: 'done',
+                            placeholderTextColor: colors.colorGreyInactive,
+                            numberOfLines: 1,
+                            multiline: false,
+                            onChangeText: (text) => { setSearchText(text) },
+                            // rightIcon: { type: 'font-awesome', name: 'chevron-left', color: 'black' },
                         }}
-                        ItemSeparatorComponent={ListItemSeparator}
-                        keyExtractor={(item, index) => index.toString()}
-                    />
-                </View>}
+                        enablePoweredByContainer={false}
+                        enableHighAccuracyLocation={true}
+                        nearbyPlacesAPI={'GoogleReverseGeocoding'}
+                        GooglePlacesDetailsQuery={{
+                            types: '(cities)',
+                        }}
+                        filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']}
+                        query={{
+                            key: config.GOOGLE_MAP_API_KEY,
+                            language: 'en',
+                            types: '(cities)',
+                        }}
+                        onPress={_onPress}
+                        renderRow={_renderRow}
+                        isRowScrollable={false}
+                        debounce={500}
+                        styles={{
+                            listView: {
+                                marginTop: scaler(20),
+                                borderTopColor: "rgba(0, 0, 0, 0.06)",
+                                borderTopWidth: 1,
+                                flex: 1,
+                                // backgroundColor:'red'
+                            },
+                            row: {
+                                padding: scaler(15),
+                            },
+                            separator: {
+                                width: Dimensions.get('screen').width - scaler(40),
+                                alignSelf: 'center',
+                            },
+                            container: {
+                            },
+                            textInputContainer: {
+                                borderTopWidth: 0,
+                                borderBottomWidth: 0,
+                                marginTop: scaler(10),
+
+                                // backgroundColor: colors.colorBlack
+                            },
+                            textInput: styles.searchInput,
+                            predefinedPlacesDescription: {
+                                color: 'white',
+                            },
+                        }}
+                        currentLocation={false}
+                    />}
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => { NavigationService.goBack() }} style={styles.closeButton} >
+                    <Image style={styles.imagePlaceholder} source={Images.ic_left} />
+                </TouchableOpacity>
+
+                {(!searchText || searchText.length == 0) &&
+                    <View style={{
+                        top: scaler(60), left: 0, right: 0, position: 'absolute', flex: 1, width: '100%',
+                        marginTop: scaler(20),
+                        // borderTopColor: "rgba(0, 0, 0, 0.06)",
+                        // borderTopWidth: 2,
+                    }} >
+                        <Text style={{
+                            fontWeight: '500', flex: 1, fontSize: scaler(12), color: '#9A9A9A',
+                            marginHorizontal: scaler(20),
+                            marginTop: scaler(20),
+                            marginBottom: scaler(10),
+                        }}>{Language.recent_search}</Text>
+                        <FlatList
+                            style={{ flex: 1 }}
+                            showsVerticalScrollIndicator={false}
+                            data={recentSearches ?? []}
+                            renderItem={({ item, index }) => {
+                                return <TouchableHighlight
+                                    onPress={() => _onPress(item?.data, item?.details, item?.otherData)}
+                                    style={{ width: '100%', padding: scaler(15) }
+                                    }
+                                    //   onPress={() => _onPress(rowData)}
+                                    underlayColor={'#c8c7cc'}
+                                >
+                                    {_renderRow(item?.data, index, Images.ic_recent_search)}
+                                </TouchableHighlight>
+                            }}
+                            ItemSeparatorComponent={ListItemSeparator}
+                            keyExtractor={(item, index) => index.toString()}
+                        />
+                    </View>}
 
 
-            {/* <TextInput style={styles.searchInput}
+                {/* <TextInput style={styles.searchInput}
                     placeholder={Language.search_placeholder}
                     placeholderTextColor={colors.colorGreyInactive}
                 />
                 <Image style={styles.imagePlaceholder} source={Images.ic_lens} /> */}
+            </View>
         </SafeAreaViewWithStatusBar >
     )
 }
@@ -192,7 +203,8 @@ const styles = StyleSheet.create({
         flexShrink: 1
     },
     searchInput: {
-        height: scaler(40), backgroundColor: '#F6F6F6',
+        height: scaler(40),
+        backgroundColor: '#F6F6F6',
         borderRadius: scaler(10),
         paddingHorizontal: scaler(45),
         paddingVertical: 0, marginVertical: 0,
@@ -200,7 +212,9 @@ const styles = StyleSheet.create({
         marginHorizontal: scaler(20),
         fontSize: scaler(12),
         fontWeight: '300',
-        color: colors.colorBlackText
+        textAlignVertical: 'center',
+        color: colors.colorBlackText,
+        // backgroundColor: 'red'
 
     },
     imagePlaceholder: {
