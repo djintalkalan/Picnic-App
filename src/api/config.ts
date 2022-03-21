6c5877ce1bf9bac2b10da084d0ac27d170849933
Deepak Jaglan <deepak@shinewebservices.com>
2022-03-21 14:12:30 +0530

    remove config changes

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,4 +1,2 @@
 import c from "react-native-ultimate-config";
-// c.API_URL = "http://ec2-44-225-46-18.us-west-2.compute.amazonaws.com/api/v1/"
-// c.SOCKET_URL = "ws://ec2-44-225-46-18.us-west-2.compute.amazonaws.com"
 export const config = c
