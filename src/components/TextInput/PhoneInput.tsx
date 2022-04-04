d597204d85b824965afd8d6f0968ad38bc83943f
Deepak Jaglan <deepak@shinewebservices.com>
2022-04-04 17:50:42 +0530

    fix

--- src/components/TextInput/PhoneInput.tsx
+++ src/components/TextInput/PhoneInput.tsx
@@ -62,6 +62,7 @@ export const PhoneInput = forwardRef((props: InputPhoneProps, ref) => {
         unmaskedPhoneNumber: "",
         phoneNumber: "",
         isVerified: true,
+        // @ts-ignore
         selectedCountry: props?.countryCode ? {
             code: props?.countryCode
         } : null
