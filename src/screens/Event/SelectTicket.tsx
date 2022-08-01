d453b62ed537de5784a70bfb5f6368b1cf59ad43
Sangeeta <sangeeta@shinewebservices.com>
2022-08-01 12:56:41 +0530

    disable conditions added

--- src/screens/Event/SelectTicket.tsx
+++ src/screens/Event/SelectTicket.tsx
@@ -4,30 +4,38 @@ import { SafeAreaViewWithStatusBar } from 'custom-components/FocusAwareStatusBar
 import React, { FC, memo, useCallback, useEffect, useState } from 'react'
 import { FlatList, Image, StyleSheet, TouchableOpacity, View } from 'react-native'
 import Language from 'src/language/Language'
-import { formatAmount, NavigationService, scaler } from 'utils'
+import { formatAmount, NavigationService, scaler, stringToDate } from 'utils'
 //@ts-ignore
 import { _totalSoldTickets } from 'api/APIProvider'
+import { RootState } from 'app-store/store'
+import { isEqual } from 'lodash'
 import ReadMore from 'react-native-read-more-text'
+import { useSelector } from 'react-redux'
 
-let ticketArray: Array<any> = []
 const SelectTicket: FC = (props: any) => {
     const [selectedTicket, setSelectedTicket] = useState<any>()
 
     const onTicketSelect = useCallback((item: any) => {
         setSelectedTicket(item)
     }, [])
+    const [allPlans, setAllPlans] = useState<any[]>([])
+
+    const { event } = useSelector((state: RootState) => ({
+        event: state?.eventDetails?.[props?.route?.params?.id]?.event,
+    }), isEqual)
+
 
     useEffect(() => {
         _totalSoldTickets({ resource_id: props?.route?.params?.id }).then(res => {
             if (res?.status == 200) {
                 console.log('res', res);
-                ticketArray = res?.data?.tickets?.filter((_: any) => { return _.status != 2 })
+                setAllPlans(res?.data?.tickets?.filter((_: any) => { return _.status != 2 }))
             } else console.log(res);
         }).catch(e => console.log(e))
     }, [])
 
     const onNextPress = useCallback(() => {
-        NavigationService.navigate('BookEvent', { id: props?.route?.params.id, selectedTicket: selectedTicket })
+        if (selectedTicket) NavigationService.navigate('BookEvent', { id: props?.route?.params.id, selectedTicket: selectedTicket })
     }, [selectedTicket])
 
     const renderTicket = useCallback(({ item, index }) => {
@@ -36,6 +44,7 @@ const SelectTicket: FC = (props: any) => {
                 isSelected={item?._id == selectedTicket?._id}
                 onPress={() => onTicketSelect(item)}
                 {...item}
+                eventDate={stringToDate(event?.event_date + " " + event?.event_start_time, 'YYYY-MM-DD', '-')}
             />
         )
     }, [selectedTicket])
@@ -47,16 +56,17 @@ const SelectTicket: FC = (props: any) => {
                     style={{ flex: 1 }}
                     contentContainerStyle={{ padding: scaler(15) }}
                     keyExtractor={_ => _?._id}
-                    data={ticketArray}
+                    data={allPlans}
                     renderItem={renderTicket}
                     ItemSeparatorComponent={() => (
                         <View style={{ flex: 1, height: scaler(10) }} />
                     )}
                 />
             </View>
-            <View style={{ marginHorizontal: scaler(10) }}>
-                <Button title={Language.next} onPress={onNextPress} />
-            </View>
+            {selectedTicket ?
+                <View style={{ marginHorizontal: scaler(10) }}>
+                    <Button title={Language.next} onPress={onNextPress} />
+                </View> : undefined}
 
         </SafeAreaViewWithStatusBar>
     )
@@ -65,8 +75,14 @@ const SelectTicket: FC = (props: any) => {
 const TicketView = memo(({ _id: id, name: title, currency, description,
     isSelected = false, amount: price, onPress,
     total_free_tickets = 0, total_free_tickets_consumed = 0,
-    sales_ends_on, capacity_type, capacity, total_sold_tickets
+    sales_ends_on, capacity_type, capacity, total_sold_tickets, eventDate
 }: any) => {
+
+    const endSales = sales_ends_on ? new Date(sales_ends_on) : eventDate;
+
+    const isDisabled = new Date() > endSales || (capacity_type == 'limited' && capacity <= total_sold_tickets)
+
+
     const _renderTruncatedFooter = (handlePress: any) => {
         return (
             <Text style={{ color: isSelected ? colors.colorBlack : colors.colorPrimary, marginTop: 5 }} onPress={handlePress}>
@@ -90,14 +106,14 @@ const TicketView = memo(({ _id: id, name: title, currency, description,
 
     return (
         <Card cardElevation={2} cornerRadius={scaler(8)}>
-            <TouchableOpacity style={[styles.renderView, isSelected ? { backgroundColor: colors.colorPrimary } : {}]} activeOpacity={0.7} onPress={onPress}>
+            <TouchableOpacity disabled={isDisabled} style={[styles.renderView, isSelected ? { backgroundColor: colors.colorPrimary } : {}]} activeOpacity={0.7} onPress={onPress}>
                 <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
-                    <Text style={[styles.mainText, { flex: 1 }, isSelected ? { color: colors.colorWhite } : {}]}>{title}</Text>
-                    <Text style={[styles.mainText, isSelected ? { color: colors.colorWhite } : {}]}>{formatAmount(currency, price)}</Text>
+                    <Text style={[styles.mainText, { flex: 1 }, isSelected ? { color: colors.colorWhite } : isDisabled ? { color: colors.colorGreyText } : {}]}>{title}</Text>
+                    <Text style={[styles.mainText, isSelected ? { color: colors.colorWhite } : isDisabled ? { color: colors.colorGreyText } : {}]}>{formatAmount(currency, price)}</Text>
                 </View>
                 {free_tickets ? <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: scaler(3), marginBottom: scaler(6) }} >
-                    <Image style={{ width: scaler(18), aspectRatio: 1, tintColor: isSelected ? colors.colorWhite : undefined }} source={Images.ic_free_ticket_icon} />
-                    <Text style={{ color: isSelected ? colors.colorWhite : colors.colorPrimary, fontSize: scaler(14) }} > {free_tickets} {Language.x_free_ticket_available}</Text>
+                    <Image style={{ width: scaler(18), aspectRatio: 1, tintColor: isSelected ? colors.colorWhite : isDisabled ? colors.colorGreyText : undefined }} source={Images.ic_free_ticket_icon} />
+                    <Text style={{ color: isSelected ? colors.colorWhite : isDisabled ? colors.colorGreyText : colors.colorPrimary, fontSize: scaler(14) }} > {free_tickets} {Language.x_free_ticket_available}</Text>
                 </View> : null}
                 <ReadMore
                     key={id}
@@ -105,7 +121,7 @@ const TicketView = memo(({ _id: id, name: title, currency, description,
                     renderTruncatedFooter={_renderTruncatedFooter}
                     renderRevealedFooter={_renderRevealedFooter}
                     onReady={_handleTextReady}>
-                    <Text style={[styles.description, isSelected ? { color: colors.colorWhite } : {}]}>{description}</Text>
+                    <Text style={[styles.description, isSelected ? { color: colors.colorWhite } : isDisabled ? { color: colors.colorGreyText } : {}]}>{description}</Text>
                 </ReadMore>
             </TouchableOpacity>
         </Card>
