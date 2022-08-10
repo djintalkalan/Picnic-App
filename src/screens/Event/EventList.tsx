9e2e9808483e520387d9b64b45ad4876b83b0166
Deepak Jaglan <deepak@shinewebservices.com>
2022-08-10 12:19:53 +0530

    removed editable restrictions

--- src/screens/Event/EventList.tsx
+++ src/screens/Event/EventList.tsx
@@ -36,7 +36,7 @@ const EventList: FC<any> = (props) => {
         const buttons: Array<IBottomMenuButton> = []
         if (is_event_admin) {
             const eventDate = new Date(item?.event_start_date_time);
-            if (!item?.total_sold_tickets && eventDate >= new Date() && item.status == 1)
+            if (eventDate >= new Date() && item.status == 1)
                 buttons.push({
                     title: Language.edit_event, onPress: () => {
                         dispatch(setActiveEvent(item))
