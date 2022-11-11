6564d9ba071285edd42f8872c879e0a80d5351d0
Deepak Jaglan <deepak@shinewebservices.com>
2022-11-11 16:58:38 +0530

    Fixed things in android

--- src/screens/Profile/PaypalDisconnect.tsx
+++ src/screens/Profile/PaypalDisconnect.tsx
@@ -1,11 +1,11 @@
-import { _updatePaypalMerchantId } from 'api';
+import { config, _updatePaypalMerchantId } from 'api';
 import { setLoadingAction } from 'app-store/actions';
 import { colors } from 'assets/Colors';
 import { MyHeader } from 'custom-components';
 import { SafeAreaViewWithStatusBar } from 'custom-components/FocusAwareStatusBar';
 import Database from 'database/Database';
-import React, { FC, useCallback, useEffect, useRef, useState } from 'react';
-import { BackHandler, StyleSheet } from 'react-native';
+import React, { FC, useCallback, useRef } from 'react';
+import { Platform, StyleSheet } from 'react-native';
 import { WebView, WebViewNavigation } from 'react-native-webview';
 import { useDispatch } from 'react-redux';
 import { NavigationService } from 'utils';
@@ -13,21 +13,47 @@ import Language from '../../language/Language';
 
 const PaypalDisconnect: FC<any> = ({ route }) => {
   const closed = useRef(false);
-  const [isBackButtonDisabled, setBackButtonDisabled] = useState(false)
+  const formSubmittedRef = useRef(false)
+  const totalChecked = useRef(0)
   const dispatch = useDispatch();
-  useEffect(() => {
-    const sub = BackHandler.addEventListener('hardwareBackPress', () => {
-      return isBackButtonDisabled
-    })
-    return sub?.remove
-  }, [isBackButtonDisabled])
 
   const onNavigationStateChange = useCallback(
     (e: WebViewNavigation) => {
+      console.log("e", e);
+
       dispatch(setLoadingAction(e?.loading));
-      if (e?.url?.endsWith('businessprofile/partner/consents') && e?.navigationType == 'formsubmit' && e?.title == 'Manage permissions') {
-        if (!closed?.current) {
-          closed.current = true;
+      if (Platform.OS == 'ios') {
+        if (e?.url?.endsWith('businessprofile/partner/consents/' + config.PAYPAL_PARTNER_ACCOUNT_ID)) {
+          formSubmittedRef.current = true
+        }
+        if (formSubmittedRef.current && e?.url?.endsWith('businessprofile/partner/consents') && e?.navigationType == 'formsubmit' && e?.title == 'Manage permissions') {
+          if (!closed?.current) {
+            closed.current = true;
+            _updatePaypalMerchantId({ paypal_merchant_id: null })
+              .then(res => {
+                if (res?.status === 200) {
+                  // _showSuccessMessage(res?.message);
+                  Database.setUserData({ ...Database.getStoredValue('userData'), paypal_merchant_id: undefined })
+                  NavigationService.goBack();
+                }
+                dispatch(setLoadingAction(false));
+              })
+              .catch(() => {
+                dispatch(setLoadingAction(false));
+              });
+          } else if (!closed?.current) {
+            dispatch(setLoadingAction(e?.loading));
+          }
+        }
+      } else {
+        if (e?.url?.endsWith('businessprofile/partner/consents')) {
+          totalChecked.current += 1
+        } else {
+          totalChecked.current = 0
+        }
+        console.log("totalChecked.current", totalChecked.current);
+
+        if (totalChecked.current > 3) {
           _updatePaypalMerchantId({ paypal_merchant_id: null })
             .then(res => {
               if (res?.status === 200) {
@@ -40,19 +66,17 @@ const PaypalDisconnect: FC<any> = ({ route }) => {
             .catch(() => {
               dispatch(setLoadingAction(false));
             });
-        } else if (!closed?.current) {
-          dispatch(setLoadingAction(e?.loading));
         }
       }
     },
     [dispatch, route?.params?.userData?._id],
   );
 
-  const actionUrl = 'https://www.sandbox.paypal.com/businessprofile/partner/consents';
+  const actionUrl = config.PAYPAL_DISCONNECT_URL;
 
   return (
     <SafeAreaViewWithStatusBar style={styles.container}>
-      <MyHeader title={Language.paypal_details} backEnabled={!isBackButtonDisabled} />
+      <MyHeader title={Language.paypal_details} />
       <WebView
         javaScriptEnabled={true}
         setDisplayZoomControls
