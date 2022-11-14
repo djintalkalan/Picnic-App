a03ed758af03c92ee4281419436fc9198533ca20
Deepak Jaglan <deepak@shinewebservices.com>
2022-11-14 18:11:13 +0530

    Updated

--- src/screens/Dashboard/Home.tsx
+++ src/screens/Dashboard/Home.tsx
@@ -1,4 +1,4 @@
-import { RootState, store } from 'app-store'
+import { RootState } from 'app-store'
 import { getAllCurrencies, getProfile, searchAtHome, setSearchedData } from 'app-store/actions'
 import { colors, Images } from 'assets'
 import { Card, defaultLocation, Text } from 'custom-components'
@@ -7,9 +7,9 @@ import ImageLoader from 'custom-components/ImageLoader'
 import TopTab, { TabProps } from 'custom-components/TopTab'
 import _, { isEqual } from 'lodash'
 import React, { FC, useCallback, useEffect, useMemo, useRef, useState } from 'react'
-import { ActivityIndicator, AppState, GestureResponderEvent, Image, ImageSourcePropType, ImageStyle, InteractionManager, StyleSheet, TextInput, TouchableOpacity, View } from 'react-native'
+import { ActivityIndicator, GestureResponderEvent, Image, ImageSourcePropType, ImageStyle, InteractionManager, StyleSheet, TextInput, TouchableOpacity, View } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
-import RNShake from 'react-native-shake'
+// import RNShake from 'react-native-shake'
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import Octicons from 'react-native-vector-icons/Octicons'
 import { useDispatch, useSelector } from 'react-redux'
@@ -72,19 +72,19 @@ const Home: FC = () => {
     })
   }, [])
 
-  useEffect(() => {
-    const shakeSubscription = RNShake.addListener(() => {
-      // Your code here...
-      console.log("Shacked", AppState.currentState)
-      if (AppState.currentState == 'active' && !store.getState()?.isLoading && !searchLoader) {
-        NavigationService.navigate("Home")
-      }
-    })
+  // useEffect(() => {
+  //   const shakeSubscription = RNShake.addListener(() => {
+  //     // Your code here...
+  //     console.log("Shacked", AppState.currentState)
+  //     if (AppState.currentState == 'active' && !store.getState()?.isLoading && !searchLoader) {
+  //       NavigationService.navigate("Home")
+  //     }
+  //   })
 
-    return () => {
-      shakeSubscription.remove()
-    }
-  }, [searchLoader])
+  //   return () => {
+  //     shakeSubscription.remove()
+  //   }
+  // }, [searchLoader])
 
 
   const insets = useSafeAreaInsets()
