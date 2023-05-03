9c9b6cd68802f9e21ca818cd3b01213ea960e8e2
Deepak Kumar <deepak@shinewebservices.com>
2023-05-03 13:11:27 +0530

    Fixed

--- src/language/es.ts
+++ src/language/es.ts
@@ -514,7 +514,7 @@ export const es = {
     poll_end_date: 'Fecha de finalización de la encuesta',
     poll_end_time: 'Hora de finalización del sondeo',
     end_date_greater_than_current: 'La fecha de finalización debe ser posterior a la fecha actual',
-    poll_ends_by_default: 'La encuesta finaliza por defecto dentro de 24 horas',
+    poll_ends_by_default: 'Seleccione la fecha y hora de finalización de la encuesta',
     ends_on: 'Finalizará el',
     poll_ended: 'Encuesta finalizada',
     at: 'a las',
